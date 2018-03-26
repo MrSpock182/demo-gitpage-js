a7f1b2ff242387d7dca177023be27478189d5672
kleber99 <kleber_nunes99@hotmail.com>
2018-03-26 18:21:50 -0300

    first

---
+++ wake.js
@@ -0,0 +1,25 @@
+function doSomething() {
+	var date = new Date();
+	var hours = date.getHours();
+	
+	console.log(date);
+	console.log(hours);
+	
+	if(hours > 8) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "https://impactaservice.herokuapp.com", true);
+		xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4) {
+				console.log("Wake herokuapp");
+			}
+		}
+		xhr.send();
+	} 
+	else {
+		console.log("Hour off");
+	}
+}
+
+doSomething();
+setInterval(doSomething, 300000);
+
